0d04896e629faea5ed86a2b567d9a520e312d653
魏文书 <weiwenshu@zoneland.net>
2021-07-10 11:42:46 +0800

    创建项目

---
+++ src/test/java/pers/oliverwee/chatroom/ChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.oliverwee.chatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
